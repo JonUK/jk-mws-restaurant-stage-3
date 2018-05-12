e9cc26d580ade70eea4c92e563b804587fa3211c
bitbucket@jjwebdesign.co.uk <cHTQOBUzBAUQb9XX>
2018-05-12 11:59:59 +0100

    Added a package.json and gruntfile.js

---
+++ gruntfile.js
@@ -0,0 +1,10 @@
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+
+    });
+
+    grunt.loadNpmTasks('grunt-responsive-images');
+
+    //grunt.registerTask('default', ['jshint']);
+};

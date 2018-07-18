8a9f5b6fd9e3c4888763a96d55df1074886be214
bitbucket@jjwebdesign.co.uk <cHTQOBUzBAUQb9XX>
2018-07-18 01:10:44 +0100

    Added placeholder image generation to the grunt-responsive-images step in preparation for adding lazy image loading support.

--- gruntfile.js
+++ gruntfile.js
@@ -1,32 +1,40 @@
 module.exports = function (grunt) {
 
-    grunt.initConfig({
-        responsive_images: {
-            dev: {
-                options: {
-                    sizes: [{
-                        width: 200,
-                        quality: 90,
-                        separator: "_"
-                    },
-                    {
-                        width: 400,
-                        quality: 90,
-                        separator: "_"
-                    }]
-                },
-                files: [{
-                    expand: true,
-                    src: ['**/*.{jpg,gif,png}'],
-                    cwd: 'img-original/',
-                    dest: 'img-export/'
-                }],
-
+  grunt.initConfig({
+    responsive_images: {
+      dev: {
+        options: {
+          sizes: [
+            {
+              name: 'placeholder',
+              width: 100,
+              quality: 20,
+              separator: "_"
+            },
+            {
+              width: 200,
+              quality: 80,
+              separator: "_"
+            },
+            {
+              width: 400,
+              quality: 80,
+              separator: "_"
             }
-        }
-    });
+          ]
+        },
+        files: [{
+          expand: true,
+          src: ['**/*.{jpg,gif,png}'],
+          cwd: 'img-original/',
+          dest: 'img-export/'
+        }],
 
-    grunt.loadNpmTasks('grunt-responsive-images');
+      }
+    }
+  });
 
-    grunt.registerTask('default', ['responsive_images']);
+  grunt.loadNpmTasks('grunt-responsive-images');
+
+  grunt.registerTask('default', ['responsive_images']);
 };

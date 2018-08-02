13389c7a19c5c884f140324fb3daa10585593db4
bitbucket@jjwebdesign.co.uk <cHTQOBUzBAUQb9XX>
2018-08-02 02:32:30 +0100

    Initial work to add favourite button on restaurant page.
    Early HTML and CSS changes to add a "Add Favourite" button on the resturant details page.

--- public/js/sw_register.js
+++ public/js/sw_register.js
@@ -3,6 +3,10 @@ if ('serviceWorker' in navigator) {
 
   console.info('Congratulations, you are running a modern browser so we can use a service worker.');
 
+  if ('SyncManager' in window) {
+    console.info('Your browser also supports background sync!');
+  }
+
   // We won't register the service worker till after the current page has loaded. We don't want our
   // service worker which can make multiple fetch requests to impact the current page load.
   window.addEventListener('load', function() {
